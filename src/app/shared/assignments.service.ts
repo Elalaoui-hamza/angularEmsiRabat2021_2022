aa97c6677a3bca7ad6917f2393ef3e8d7a2411fe
Elalaoui Hamza <hamzaelalaoui8@gmail.com>
2022-01-24 14:05:19 +0100

    changement url

--- src/app/shared/assignments.service.ts
+++ src/app/shared/assignments.service.ts
@@ -16,8 +16,8 @@ export class AssignmentsService {
   constructor(private loggingService: LoggingService, private http:HttpClient) { }
 
   //url = "http://localhost:8010/api/assignments";
-  //url = "https://api-emsi-rabat-2022.herokuapp.com/api/assignments";
-  url = "https://api-cours-angular-hamza2022.herokuapp.com/api/assignments";
+  url = "https://api-emsi-rabat-2022.herokuapp.com/api/assignments";
+  url2 = "https://api-cours-angular-hamza2022.herokuapp.com/api/assignments";
 
   getAssignments():Observable<Assignment[]> {
     return this.http.get<Assignment[]>(this.url);
